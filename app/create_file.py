a355f41c672f928f4d0d05908b2cc6c7fa5c29dc
Daniil Baianov <bayanovdanil666@gmail>
2026-05-28 18:54:35 +0200

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,37 @@
-# write your code here
+import sys
+import os
+from datetime import datetime
+
+
+def create_file(args=None) -> None:
+    if args is None:
+        args = sys.argv[1:]
+    directory = []
+    file_name = ""
+    for index, element in enumerate(args):
+        if element == "-f" and index + 1 < len(args) and not args[index + 1].startswith("-"):
+            file_name = args[index + 1]
+        if element == "-d":
+            next_index = index + 1
+            while next_index < len(args) and not args[next_index].startswith("-"):
+                directory.append(args[next_index])
+                next_index += 1
+    if directory:
+        os.makedirs(os.path.join(*directory), exist_ok=True)
+    if file_name == "":
+        return
+    full_path = os.path.join(*directory, file_name)
+    first_line = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(full_path, "a") as sours_file:
+        sours_file.write(first_line + "\n")
+        counter = 1
+        while True:
+            line = input("Enter content line: ")
+            if line == "stop":
+                break
+            sours_file.write(f"{counter} {line}\n")
+            counter += 1
+
+
+if __name__ == "__main__":
+    create_file()
