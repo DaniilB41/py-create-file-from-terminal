95a842bbd2c82550f2d99fcb18e7a0dbd970ffab
Daniil Baianov <bayanovdanil666@gmail>
2026-06-20 11:28:37 +0200

    Solution 2.3

--- app/create_file.py
+++ app/create_file.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 
-def create_file(args: tuple) -> None:
+def create_file(args: tuple | None = None) -> None:
     if args is None:
         args = sys.argv[1:]
     if not args:
